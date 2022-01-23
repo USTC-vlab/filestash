57d7fd97c20a0c16fdc391e28e237dc24d02ea38
taoky <taoky1999@gmail.com>
2022-01-23 23:00:11 +0800

    Remove nonexist plg_starter_tunnel

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -1,22 +1,21 @@
 package plugin
 
 import (
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_starter_tunnel"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_starter_tor"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_video_transcoder"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_editor_onlyoffice"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_syncthing"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_image_light"
+	. "github.com/mickael-kerjean/filestash/server/common"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_backblaze"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_dav"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_mysql"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_ldap"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_dropbox"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_ldap"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_mysql"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_vlab"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_editor_onlyoffice"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_console"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_syncthing"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_image_light"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_scanner"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_console"
-	. "github.com/mickael-kerjean/filestash/server/common"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_starter_tor"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_video_transcoder"
 )
 
 func init() {

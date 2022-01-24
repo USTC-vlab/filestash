2d304a773762afe7094c3738d2a86758bb3ae5cd
taoky <taoky1999@gmail.com>
2022-01-24 19:25:24 +0800

    Set correct permission when mkdir
    Who would not like to set 'x' to dir when mkdir?

--- server/plugin/plg_backend_local/index.go
+++ server/plugin/plg_backend_local/index.go
@@ -58,7 +58,7 @@ func (this Local) Cat(path string) (io.ReadCloser, error) {
 }
 
 func (this Local) Mkdir(path string) error {
-	return os.Mkdir(path, 0664)
+	return os.Mkdir(path, 0774)
 }
 
 func (this Local) Rm(path string) error {

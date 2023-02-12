8329a8543a8c7f804a3650ce34f9cf3018c4e9e5
taoky <me@taoky.moe>
2023-02-13 03:24:38 +0800

    nix: now can use normal nodejs16

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 with pkgs; mkShell {
     buildInputs = [
-        nodejs-16_x-openssl_1_1  # webpack needs old openssl 1.1
+        nodejs-16_x
         go
         vips  # image processing support
         pkg-config
